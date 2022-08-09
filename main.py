232a280c5aeb40c88fedc9b70cbec500fc5029f4
risjain24 <rishabhjain240900@gmail.com>
2022-08-09 17:15:53 +0530

    Recovered Cases Feature Added

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Data:
 		results = [{headers[index]:cell.text for index,cell in enumerate(row.find_all("td"))  } for row in table_rows]
 		main_counter = soup.find_all("div",class_="maincounter-number")
 		counter_list = [counter.text.strip() for counter in main_counter]
-		result_total = [{'name':'Coronavirus Cases:','value':counter_list[0]}, {'name':'Deaths:','value':counter_list[1]}]
+		result_total = [{'name':'Coronavirus Cases:','value':counter_list[0]}, {'name':'Deaths:','value':counter_list[1]}, {'name':'Recovered:','value':counter_list[2]}]
 		self.data = {'total': result_total, 'country': results}
 
 	def get_TotalCases(self):
@@ -32,6 +32,15 @@ class Data:
 
 		for content in data:
 			if content['name'] == "Deaths:":
+				return content['value']
+
+		return "0"
+
+	def get_TotalRecovered(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == "Recovered:":
 				return content['value']
 
 		return "0"
@@ -86,20 +95,28 @@ def main():
 					re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_TotalDeaths,
 					re.compile("[\w\s]+ total deaths"): data.get_TotalDeaths,
 					re.compile("[\w\s]+ total [\w\s]+ death"): data.get_TotalDeaths,
-					re.compile("[\w\s]+ total death"): data.get_TotalDeaths
+					re.compile("[\w\s]+ total death"): data.get_TotalDeaths,
+					re.compile("[\w\s]+ total [\w\s]+ recovered"): data.get_TotalRecovered,
+					re.compile("[\w\s]+ total recovered"): data.get_TotalRecovered
 					}
 
 	COUNTRY_PATTERNS = {
 						re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['TotalCases'],
                     	re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['TotalDeaths'],
-                    	re.compile("[\w\s]+ death [\w\s]+"): lambda country: data.get_country_data(country)['TotalDeaths']
+                    	re.compile("[\w\s]+ death [\w\s]+"): lambda country: data.get_country_data(country)['TotalDeaths'],
+						re.compile("[\w\s]+ recovered [\w\s]+"): lambda country: data.get_country_data(country)['TotalRecovered']
                     	}
 
 	while True:
 		speak("Listening...")
 		text = get_audio()
 		print(text)
+		text = "Query " + text
 		result = None
+
+		if text.find(END_PHRASE) != -1:
+			speak(END_MSG)
+			return
 
 		for pattern, func in COUNTRY_PATTERNS.items():
 			if pattern.match(text):
@@ -116,9 +133,5 @@ def main():
 
 		if result:
 			speak(result)
-
-		if text.find(END_PHRASE) != -1:
-			speak(END_MSG)
-			break
 
 main()
